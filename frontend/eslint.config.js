8875a8f503d5e1d9e95f7ad6db9be6b8b6126940
mukahbaw <sawmukahbaw191@gmail.com>
2026-01-22 03:17:02 +1300

    DTE

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,17 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
+import { FlatCompat } from '@eslint/eslintrc'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const compat = new FlatCompat({ baseDirectory: __dirname })
 
 export default defineConfig([
   globalIgnores(['dist', 'src', '.next']),
+  ...compat.extends('next/core-web-vitals'),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
